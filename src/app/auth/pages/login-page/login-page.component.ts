946b8684ed73b46914466e13c5ca59d9be11ecf9
davidfernndez775 <davidfernndez775@yahoo.com>
2024-12-01 10:26:31 -0500

    routing done

---
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css',
+})
+export class LoginPageComponent {}
---
+++ src/app/dashboard/layout/dashboard-layout/dashboard-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './dashboard-layout.component.html',
+  styleUrl: './dashboard-layout.component.css',
+})
+export class DashboardLayoutComponent {}
